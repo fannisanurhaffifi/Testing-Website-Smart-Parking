fce3c1108e00c3e1d6348a98d99e8304bd7a1f7e
fannisanurhaffifi <fannisanurhaffifi@gmail.com>
2026-02-19 02:47:17 +0700

    perbaikan database

--- backend/src/config/database.sql
+++ backend/src/config/database.sql
@@ -4,11 +4,11 @@
 
 show databases;
 
-CREATE DATABASE IF NOT EXISTS smartpark;
+CREATE DATABASE IF NOT EXISTS parkir_db;
 
-drop database if exists smartpark;
+drop database if exists parkir_db;
 
-USE smartpark;
+USE parkir_db;
 
 -- =========================
 -- TABEL PENGGUNA
@@ -17,10 +17,10 @@ CREATE TABLE pengguna (
     npm VARCHAR(50) PRIMARY KEY,
     nama VARCHAR(50) NOT NULL,
     email VARCHAR(50) UNIQUE,
-    jurusan VARCHAR(100),
-    prodi VARCHAR(100),
-    angkatan VARCHAR(50),
-    foto VARCHAR(100),
+    jurusan VARCHAR(100) NOT NULL,
+    prodi VARCHAR(100) NOT NULL,
+    angkatan VARCHAR(50) NOT NULL,
+    foto VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
     status_akun BOOLEAN DEFAULT FALSE,
     tanggal_daftar DATETIME
@@ -50,7 +50,20 @@ CREATE TABLE kendaraan (
         ON DELETE CASCADE
 );
 
-
+-- =========================
+-- TABEL RFID
+-- =========================
+CREATE TABLE rfid (
+    id_rfid INT AUTO_INCREMENT PRIMARY KEY,
+    id_kendaraan INT NOT NULL,
+    kode_rfid VARCHAR(50) UNIQUE NOT NULL,
+    status_rfid BOOLEAN DEFAULT TRUE,
+    tanggal_aktif DATETIME,
+    CONSTRAINT fk_rfid_kendaraan
+        FOREIGN KEY (id_kendaraan)
+        REFERENCES kendaraan(id_kendaraan)
+        ON DELETE CASCADE
+);
 
 -- =========================
 -- TABEL SLOT PARKIR
@@ -117,3 +130,19 @@ CREATE TABLE reset_password_otp (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE rfid_registration_session (
+    id_session INT AUTO_INCREMENT PRIMARY KEY,
+    id_kendaraan INT NOT NULL,
+    id_admin INT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    expired_at DATETIME,
+    status ENUM('PENDING','DONE','EXPIRED') DEFAULT 'PENDING',
+    CONSTRAINT fk_session_kendaraan
+        FOREIGN KEY (id_kendaraan)
+        REFERENCES kendaraan(id_kendaraan)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_session_admin
+        FOREIGN KEY (id_admin)
+        REFERENCES admin(id_admin)
+        ON DELETE CASCADE
+);
