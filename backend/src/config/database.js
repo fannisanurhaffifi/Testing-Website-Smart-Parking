21aeef4b99fd27d6b6dac8a63da3945b1c57961d
fannisanurhaffifi <fannisanurhaffifi@gmail.com>
2026-02-19 02:51:23 +0700

    perbaikan database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,7 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-console.log(`📡 Connecting to DB at ${process.env.DB_HOST}:${process.env.DB_PORT}, Database: ${process.env.DB_NAME}`);
-
+console.log(`📡 Connecting to DB at ${process.env.MYSQLHOST}:${process.env.MYSQLPORT}, Database: ${process.env.MYSQL_DATABASE}`);
 
 const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
@@ -14,37 +13,13 @@ const pool = mysql.createPool({
   connectionLimit: 10,
 });
 
-
 const connectToDatabase = async () => {
   try {
     const conn = await pool.getConnection();
     console.log("✅ Database connected successfully");
-
-    // Auto Create Table if missing
-    await conn.execute(`
-      CREATE TABLE IF NOT EXISTS rfid_registration_session (
-        id_session INT AUTO_INCREMENT PRIMARY KEY,
-        id_kendaraan INT NOT NULL,
-        id_admin INT NOT NULL,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        expired_at DATETIME,
-        status ENUM('PENDING','DONE','EXPIRED') DEFAULT 'PENDING',
-        CONSTRAINT fk_session_kendaraan
-            FOREIGN KEY (id_kendaraan)
-            REFERENCES kendaraan(id_kendaraan)
-            ON DELETE CASCADE,
-        CONSTRAINT fk_session_admin
-            FOREIGN KEY (id_admin)
-            REFERENCES admin(id_admin)
-            ON DELETE CASCADE
-      )
-    `);
-    console.log("✅ Table 'rfid_registration_session' is ready");
-
     conn.release();
   } catch (err) {
-    console.error("❌ Database connection failed FULL ERROR:", err);
-
+    console.error("❌ Database connection failed:", err.message);
     process.exit(1);
   }
 };
