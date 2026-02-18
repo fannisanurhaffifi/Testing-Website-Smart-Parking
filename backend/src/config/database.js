1bd8dc25cf902532004cf2457ab9a53f95846998
fannisanurhaffifi <fannisanurhaffifi@gmail.com>
2026-02-19 02:44:14 +0700

    perbaikan nama database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -8,12 +8,11 @@ const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
 });
-
 
 
 const connectToDatabase = async () => {
